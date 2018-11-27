a5c661590e70de6afe8dc5a549c26dd0c637de80
Dewald Henning <dewaldhenning@hotmail.com>
2018-11-27 11:16:45 +1100

    26/11

---
+++ SIM_to_GL_script_ex01.sql
@@ -0,0 +1,4 @@
+Select * from YBBSIM Where YBATNBR_0 = 'BB20181100083' and YTRANSNBR_0 = 1
+--> FCY
+Declare SITE_COUNT
+Select distinct YFCY_0 from YBBSIM Where YBATNBR_0 = 'BB20181100083' and YTRANSNBR_0 
